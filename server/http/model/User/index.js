21a9f2e8b038f2c6d2f62c65e8865c14e6a6a300
Luis <webdeveloperpr@gmail.com>
2018-02-04 01:53:49 -0500

    Update schema

--- server/http/model/User/index.js
+++ server/http/model/User/index.js
@@ -8,6 +8,8 @@ const UserSchema = Schema({
     type: String,
     validate: validation.username,
     unique: true,
+    sparse: true,
+    trim: true,
   },
   email: {
     type: String,
@@ -15,17 +17,21 @@ const UserSchema = Schema({
     validate: validation.email,
     unique: true,
     index: true,
+    trim: true,
   },
   password: {
     type: String,
     required: [true, 'Field is required'],
     validate: validation.password,
+    trim: true,
   },
   firstName: {
     type: String,
+    trim: true,
   },
   lastName: {
     type: String,
+    trim: true,
   },
   birthday: {
     type: Date,
@@ -33,21 +39,26 @@ const UserSchema = Schema({
   phone: {
     type: String,
     validate: validation.phone,
+    trim: true,
   },
   zipCode: {
     type: String,
     validate: validation.zipCode,
+    trim: true,
   },
   city: {
     type: String,
+    trim: true,
   },
   state: {
     type: String,
     validate: validation.state,
+    trim: true,
   },
   country: {
     type: String,
     validate: validation.country,
+    trim: true,
   },
 }, { timestamps: true });
 
