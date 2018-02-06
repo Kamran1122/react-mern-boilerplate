abfd5b8f74806aafad3521fbe2fbe77db3f5d8a0
Luis <webdeveloperpr@gmail.com>
2018-02-06 00:54:18 -0500

    Organize reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,6 +1,6 @@
-import location from './location'
 import auth from './auth'
 import user from './user'
+import location from './location'
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 
